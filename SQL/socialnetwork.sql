e313bd53591f21b996cd9e2e63fe6ba65eb6c4ca
MEYOUS <meyous.projets@gmail.com>
2023-05-09 22:40:07 +0000

    La base de données

---
+++ SQL/socialnetwork.sql
@@ -0,0 +1,39 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : mar. 09 mai 2023 à 22:33
+-- Version du serveur : 8.0.31
+-- Version de PHP : 8.0.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `socialnetwork`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ config/database.php
@@ -0,0 +1,31 @@
+<?php
+
+// Checking drives availables
+
+/* var_dump(PDO::getAvailableDrivers());
+  exit();
+*/
+
+// Database credentials
+
+define('DB_HOST','localhost');
+define('DB_NAME','socialnetwork');
+define('DB_USERNAME','root');
+define('DB_PASSWORD','');
+
+try {
+
+    $db = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USERNAME,DB_PASSWORD);
+
+    //$db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_SILENCE);
+
+    //$db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_WARNING);
+
+    $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+}
+catch(PDOException $e){
+
+    die('Erreur : '.$e->getMessage());
+
+}
