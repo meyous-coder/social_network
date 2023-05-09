cbe8253f2a0b840cc465cc72bd28abe9e54909d0
MEYOUS <meyous.projets@gmail.com>
2023-05-09 22:05:52 +0000

    Page d'accueil

--- views/index.view.php
+++ views/index.view.php
@@ -1,27 +1,75 @@
-<!doctype html>
-<html lang="fr">
+
+<!DOCTYPE html>
+<html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Formulaire</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="Réseau social pour développeur">
+    <meta name="author" content="MEITE YOUSSOUF">
+
+    <title>Boom Social Network</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="assets/css/main.css" rel="stylesheet">
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="https://cdn.jsdelivr.net/npm/html5shiv@3.7.3/dist/html5shiv.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/respond.js@1.4.2/dest/respond.min.js"></script>
+    <![endif]-->
 </head>
+
 <body>
-<form action="" method="post">
-    <pre>
-        <label for="lastname">Nom</label>
-        <input type="text" id="lastname" name="lastname">
 
-        <label for="firstname">Prénoms</label>
-        <input type="text" id="firstname" name="firstname">
+<nav class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Boom Social Network</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Accueil</a></li>
+                <li><a href="#about">Inscription</a></li>
+                <li><a href="#contact">Connexion</a></li>
+            </ul>
+        </div><!--/.nav-collapse -->
+    </div>
+</nav>
 
-        <label for="message">Message</label>
-        <textarea name="message" id="" cols="30" rows="10"></textarea>
+<div class="container">
+
+    <div class="jumbotron">
+        <h1>Bootstrap starter template</h1>
+        <p>
+            Boom Social Network est le réseau social des développeurs &#9000; <br>
+            Et qui dit développeurs, dit également code source! &#9997;<br>
+            Grâce à cette plateforme, vous avez la possibilité de tisser des Liens
+            d'amitiés avec d'autres développeurs, échanger des codes soucres,
+            recevoire de l'aide si vous rencontrez  <br>
+            des problèmes sur l'un de vos projets et bien plus encore !  <br>
+            Alors n'hésitez plus et <a href='register.php'>rejoignez dès maintenant la communauté Boom </a>!&#9786;
+        </p>
+        <a href='register.php' class='btn btn-primary btn-lg'> Créer un compte</a>
+    </div>
+
+</div><!-- /.container -->
 
 
-        <input type="submit" name="envoyer" value="Envoyer">
-    </pre>
-</form>
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="assets/js/jquery1.12.4.js"></script>
+<script src="assets/js/bootstrap.js"></script>
+
 </body>
 </html>
