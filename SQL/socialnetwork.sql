4b1ffd115570ef3ce78fe0a2658bf32b87664b57
MEYOUS <meyous.projets@gmail.com>
2023-05-28 02:00:33 +0000

    Page de profil (Partie 1)

--- SQL/socialnetwork.sql
+++ SQL/socialnetwork.sql
@@ -2,10 +2,10 @@
 -- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1:3306
--- Généré le : jeu. 18 mai 2023 à 18:06
--- Version du serveur : 8.0.31
--- Version de PHP : 8.0.26
+-- Host: 127.0.0.1:3306
+-- Generation Time: May 28, 2023 at 01:58 AM
+-- Server version: 8.0.31
+-- PHP Version: 8.0.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données : `socialnetwork`
+-- Database: `socialnetwork`
 --
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `users`
+-- Table structure for table `users`
 --
 
 DROP TABLE IF EXISTS `users`;
@@ -35,17 +35,24 @@ CREATE TABLE IF NOT EXISTS `users` (
   `email` varchar(60) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `password` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `active` enum('0','1') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '0',
+  `city` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `country` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `sex` enum('H','F') COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `twitter` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `github` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `available_for_hiring` enum('0','1') COLLATE utf8mb4_unicode_ci DEFAULT '0',
+  `bio` text COLLATE utf8mb4_unicode_ci,
   PRIMARY KEY (`id`),
   UNIQUE KEY `pseudo` (`pseudo`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Déchargement des données de la table `users`
+-- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `pseudo`, `email`, `password`, `active`) VALUES
-(1, NULL, 'meyous', 'meyous2023@gmail.com', NULL, '0');
+INSERT INTO `users` (`id`, `name`, `pseudo`, `email`, `password`, `active`, `city`, `country`, `sex`, `twitter`, `github`, `available_for_hiring`, `bio`) VALUES
+(13, 'MEITE Youssouf', 'meyous-coder', 'meyous2023@gmail.com', '7c4a8d09ca3762af61e59520943dc26494f8941b', '0', NULL, NULL, NULL, NULL, NULL, '0', NULL);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- views/profile.view.php
+++ views/profile.view.php
@@ -2,8 +2,131 @@
 
     <div class="container">
 
-            <h1 class="lead">Page de profil </h1>
-            <?php include "partials/_errors.php" ?>
+
+        <div class="row">
+            <div class="col-md-6">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Profil de : <?= e( $_SESSION['pseudo'])?></h3>
+                    </div>
+                    <div class="panel-body">
+                        Image <br>
+                        Pseudo <br>
+                        Adresse email <br>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Complèter mon profil</h3>
+                    </div>
+                    <div class="panel-body">
+                        <?php include('partials/_errors.php'); ?>
+                        <form data-parsley-validate method="post" autocomplete="off">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="name">Nom
+                                            <small class="text-danger">*</small>
+                                        </label>
+                                        <input type="text" name="name" id="name" class="form-control"
+                                               required="required" value=""/>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="city">Ville
+                                            <small class="text-danger">*</small>
+                                        </label>
+                                        <input type="text" name="city" id="city" class="form-control"
+                                               required="required" value=""/>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <label for="avatar">Changer mon avatar</label>
+                                        <input id="avatar" type="file" name="avatar"/>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="country">Pays
+                                            <small class="text-danger">*</small>
+                                        </label>
+                                        <input type="text" name="country" id="country" class="form-control"
+                                               required="required" value=""/>
+                                    </div>
+                                </div>
+
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="sex">Sexe
+                                            <small class="text-danger">*</small>
+                                        </label>
+                                        <select type="text" name="sex" id="sex" class="form-control"
+                                                required="required">
+                                            <option value="H" >
+                                                Homme
+                                            </option>
+                                            <option value="F" >
+                                                Femme
+                                            </option>
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="twitter">Twitter</label>
+                                        <input type="text" name="twitter" id="twitter" class="form-control" value=""/>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="github">Github</label>
+                                        <input type="text" name="github" id="github" class="form-control" value=""/>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <label for="available_for_hiring">
+                                            <input type="checkbox" name="available_for_hiring" id="available_for_hiring"/>
+                                            Disponible pour emploi ?
+                                        </label>
+
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <label for="twitter">Biographie<span class="text-danger">*</span></label>
+                                        <textarea name="bio" id="bio" cols="30" rows="10" required="required"
+                                                  class="form-control"
+                                                  placeholder="Je suis un amoureux de la programmation..."></textarea>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <input type="submit" class="btn btn-primary" value="Valider" name="update" >
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
 
 
     </div><!-- /.container -->
