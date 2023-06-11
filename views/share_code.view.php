b2e5a65ec17c92eec86ca5dfe7cfc70f7740ca69
MEYOUS <meyous.projets@gmail.com>
2023-06-11 03:39:50 +0000

    Activation de la tabulation dans un textarea

--- views/share_code.view.php
+++ views/share_code.view.php
@@ -6,11 +6,21 @@
                 <textarea name="code" id="code" placeholder="Entrer votre code ici ..." required ></textarea>
 
                 <div class="btn-group nav">
-                    <a href="#" class="btn btn-danger">Tout effacer</a>
+<!--                    <a href="share_code.view.php" class="btn btn-danger">Tout effacer</a>-->
+                    <input type="reset"  class="btn btn-danger" value="Tout effacer">
                     <input type="submit" name="save" class="btn btn-success" value="Enregistrer">
                 </div>
             </form>
         </div>
     </div><!-- /.container -->
 
-<?php include "partials/_footer.php" ?>
+
+<script src="assets/js/jquery1.12.4.js"></script>
+<script src="assets/js/bootstrap.js"></script>
+<script src="assets/js/tabby.min.js"></script>
+<script>
+    $("#code").tabby();
+    $("#code").height($(window).height()-50);
+</script>
+</body>
+</html>
