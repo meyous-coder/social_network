87299f7886e72c587eb6593a05f98697f90021b0
MEYOUS <meyous.projets@gmail.com>
2023-08-10 22:57:11 +0000

    Pagination de la liste des utilisateurs

--- list_users.php
+++ list_users.php
@@ -15,10 +15,69 @@ $users = $q->fetchAll(PDO::FETCH_OBJ);
 
 // var_dump($_SESSION);
 // die();
-
-
 /*****************************************************************/
 
+$req = $db->query("SELECT id FROM users WHERE active='1'");
+
+$nbre_total_users = $req->rowCount();
+
+$nbre_users_par_page = 12;
+
+$nbre_pages_max_gauche_et_droite = 4;
+
+$last_page = ceil($nbre_total_users / $nbre_users_par_page);
+
+if(isset($_GET['page']) && is_numeric($_GET['page'])){
+    $page_num = $_GET['page'];
+} else {
+    $page_num = 1;
+}
+
+if($page_num < 1){
+    $page_num = 1;
+} else if($page_num > $last_page) {
+    $page_num = $last_page;
+}
+
+$limit = 'LIMIT '.($page_num - 1) * $nbre_users_par_page. ',' . $nbre_users_par_page;
+
+//Cette requête sera utilisée plus tard
+$q = $db->query("SELECT id,pseudo, email FROM users ORDER BY pseudo ASC $limit");
+$users = $q->fetchAll(PDO::FETCH_OBJ);
+
+$pagination = '<nav class="text-center"><ul class="pagination">';
+/**
+ * Si la page est égale à 1 alors traitement de la pagination
+ *
+ */
+if($last_page != 1){
+    if($page_num > 1){
+        $previous = $page_num - 1;
+        $pagination .= '<li><a href="list_users.php?page='.$previous.'">Précédent</a></li>';
+
+        for($i = $page_num - $nbre_pages_max_gauche_et_droite; $i < $page_num; $i++){
+            if($i > 0){
+                $pagination .= '<li><a href="list_users.php?page='.$i.'">'.$i.'</a></li>';
+            }
+        }
+    }
+
+    $pagination .= '<li class="active"><a href="#">'.$page_num.'</a></li>';
+
+    for($i = $page_num+1; $i <= $last_page; $i++){
+        $pagination .= '<li><a href="list_users.php?page='.$i.'">'.$i.'</a> </li>';
+
+        if($i >= $page_num + $nbre_pages_max_gauche_et_droite){
+            break;
+        }
+    }
+
+    if($page_num != $last_page){
+        $next = $page_num + 1;
+        $pagination .= '<li><a href="list_users.php?page='.$next.'">Suivant</a></li> ';
+    }
+}
+$pagination .= '</nav></ul>';
 /*****************************************************************/
 require "views/list_users.view.php";
 /*****************************************************************/
--- views/list_users.view.php
+++ views/list_users.view.php
@@ -1,17 +1,22 @@
 <?php include "partials/_header.php" ?>
-<div class="container">
-  <h1 class="lead">Liste des utilisateurs</h1>
-  <?php foreach ($users as $user): ?>
-    <div class="user_block col-md-3">
-      <a href="profile.php?id=<?= $user->id ?>">
-        <img src="<?= get_avatar_url($user->email, 80) ?>" class="img-circle">
-      </a>
-      <a href="profile.php?id=<?= $user->id ?>">
-        <h4 class="user_block_username">
-          <?= $user->pseudo ?>
-        </h4>
-      </a>
-    </div>
-  <?php endforeach; ?>
-</div><!-- /.container -->
+    <div class="container">
+        <h1 class="lead">Liste des utilisateurs</h1>
+        <?php foreach (array_chunk($users, 4) as $user_set): ?>
+            <div class="row users">
+                <?php foreach ($user_set as $user): ?>
+                    <div class="user_block col-md-3">
+                        <a href="profile.php?id=<?= $user->id ?>">
+                            <img src="<?= get_avatar_url($user->email, 80) ?>" class="img-circle avatar">
+                        </a>
+                        <a href="profile.php?id=<?= $user->id ?>">
+                            <h4 class="user_block_username">
+                                <?= $user->pseudo ?>
+                            </h4>
+                        </a>
+                    </div>
+                <?php endforeach; ?>
+            </div>
+        <?php endforeach; ?>
+        <div id="pagination"><?= $pagination ?></div>
+    </div><!-- /.container -->
 <?php include "partials/_footer.php" ?>
